5f5d402d7967da66485e66b2f4d86f7695952f6d
mbam peaceful <114789875+wobyeb@users.noreply.github.com>
2025-05-10 00:13:17 +0100

    Fix hydration error by adding suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`dark ${monaSans.variable}`}>
+    <html lang="en" className={`dark ${monaSans.variable}`} suppressHydrationWarning>
       <body className="antialiased pattern">
         {children}
         <Toaster />
